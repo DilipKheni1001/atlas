9882c0cd6e84b249a617b26e446f6a359f895c31
DilipKheni10001 <discussionforall@gmail.com>
2021-07-01 18:10:05 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
+import "./App.css";
 import Header1 from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
@@ -13,24 +13,19 @@ import {
   Link,
   NavLink,
 } from "react-router-dom";
-import Home2_PDF from "./components/Home2_PDF";
 
 function App() {
   return (
     <div className="App">
       <Router>
         {/* <Header1 /> */}
-        {/* <Sidebar /> */}
-
+        <Sidebar />
         <Switch>
           <Route path="/home">
             <Home />
           </Route>
           <Route path="/home_2">
             <Home_2 />
-          </Route>
-          <Route path="/home2_pdf">
-            <Home2_PDF />
           </Route>
         </Switch>
       </Router>
